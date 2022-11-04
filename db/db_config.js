afe62b4616a56331417d1f322f81fc8e5ae26c8b
Matheus <mattt4016@gmail.com>
2022-11-04 19:57:36 -0300

    API

---
+++ db/db_config.js
@@ -0,0 +1,27 @@
+const Pool = require('pg').Pool;
+require('dotenv').config()
+
+const env = process.env.NODE_ENV || 'Dev';
+
+let connection = {};
+
+if (env !== 'Dev') {
+    connection = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+} else {
+    connection = {
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        localhost: process.env.DB_HOST,
+        password: process.env.DB_PASSWORD,
+        PORT: process.env.DB_PORT
+    }
+}
+
+const pool = new Pool(connection)
+
+module.exports = pool;
